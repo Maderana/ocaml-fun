29855f01dcafab0b23cd8af347b369efc0e4494f
Prakhar Srivastav <prakhar1989@gmail.com>
2016-01-15 03:01:56 -0500

    finished recursive types

---
+++ week3/recursive_types.ml
@@ -0,0 +1,79 @@
+let rec length = function
+  | [] -> 0
+  | x :: xs -> 1 + length xs
+;;
+
+let rec rev = function
+  | [] -> []
+  | x :: xs -> rev xs @ [ x ] (* list concat *)
+;;
+
+let rec rev_aux accum = function 
+  | [] -> accum
+  | x :: xs -> rev_aux (x :: accum) xs
+;;
+
+let faster_rev l = rev_aux [] l;;
+
+type queue = int list * int list;;
+let q : queue = ([1; 2; 3], [4; 5; 6]);;
+
+let is_empty ((a, b): queue) = 
+  match (List.length a, List.length b) with
+  | (0, 0) -> true
+  | _ -> false
+;;
+    
+let elements ((a, b): queue) : int list = 
+  a @ List.rev b
+;;
+
+let enqueue (x: int) ((front, back): queue) : queue = 
+  (front, x :: back)
+;;
+
+(* return first n items of l *)
+let rec first_n l n accum = 
+  if n = 0 then List.rev accum
+  else first_n (List.tl l) (n - 1) (List.hd l :: accum)
+;;
+
+let split (xs: int list) : queue =
+  let is_even = List.length xs mod 2 = 0 in
+  let mid = List.length xs / 2 in
+  let front = first_n xs (if is_even then mid else mid + 1) [] in
+  let back = first_n (List.rev xs) mid [] in
+  (front, back)
+;;
+
+let dequeue ((front, back): queue) : (int * queue) =
+  (List.hd front, (List.tl front, back))
+;;
+
+let rec mem x l =
+ match l with 
+   | [] -> false
+   | y :: ys -> if x = y then true else mem x ys
+;;
+
+let rec append l1 l2 = 
+  match l1 with 
+  | [] -> l2
+  | x :: xs -> x :: append xs l2
+;;
+
+let rec combine l1 l2 = 
+  match (l1, l2) with
+  | ([], []) -> []
+  | (xs, []) -> [] (* these two patterns don't matter ... *)
+  | ([], ys) -> [] (* ... since the lists are equal *)
+  | (x :: xs, y :: ys) -> (x, y) :: combine xs ys
+;;
+
+let rec assoc (l : (string * int) list) (k: string) : int option = 
+  match l with 
+  | [] -> None
+  | (a, b) :: xs -> if k = a then Some b else assoc xs k
+;;
+
+let result = assoc [("beta", 24); ("alpha", 43); ("gamma", 13)] "alpha";;
--- week3/tagged_values.ml
+++ week3/tagged_values.ml
@@ -11,3 +11,58 @@ let lighter (c1: color) (c2: color) =
   | (Gray, Black) -> false
   | (White, Black) -> true
 ;;
+
+let find (arr: string array) (s: string) : int option =
+  let rec loop i = 
+    if i >= Array.length arr
+    then None
+    else if arr.(i) = s
+    then Some i
+    else loop (i + 1) in
+  loop 0
+;;
+
+let default_int (o : int option) : int = 
+  match o with 
+  | None -> 0
+  | Some x -> x
+;;
+
+let merge (op1: int option) (op2: int option) : int option = 
+  match (op1, op2) with
+  | (None, None) -> None
+  | (Some x, None) -> Some x
+  | (None, Some y) -> Some y
+  | (Some x, Some y) -> Some (x + y)
+;;
+
+type index = Index of int;;
+
+let read (arr : int array) (i: index) : int =
+  match i with
+    Index x -> arr.(x)
+;;
+
+let inside (arr: int array) (i: index) : bool =
+  match i with 
+    Index x -> x < (Array.length arr) && x >= 0
+;;
+
+let next i =
+  match i with
+    Index x -> Index (x + 1)
+;;
+
+let get_minindex (arr : int array) : int = 
+  let (i, _) = 
+    Array.fold_left 
+    (fun (i1,x1) (i2,x2) -> if x1 < x2 then (i1,x1) else (i2, x2)) 
+    (-1, max_int)
+    (Array.mapi (fun i x -> (i, x)) arr) in
+  i
+;;
+
+let min_index (arr: int array) : index =
+  Index (get_minindex arr)
+;;
+
