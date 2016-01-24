a71013d694e170c716de2fdeda9643c6010ed6b8
Prakhar Srivastav <prakhar1989@gmail.com>
2016-01-24 13:52:58 -0500

    faster implementation

--- misc/randomtext.ml
+++ misc/randomtext.ml
@@ -3,11 +3,20 @@ open Hashtbl;;
 
 type ltable = (string * string list) list;;
 
+type distribution =
+  { total : int;
+    amounts : (string * int) list }
+;;
+
+type htable = (string, distribution) Hashtbl.t;;
+
 let words s = Str.split (Str.regexp "[^a-zA-Z0-9]+") s;;
 
 let text = "I am a man and my dog is a good dog and a good dog makes a good man";;
 
 Random.init 123;;
+
+(* using association lists *)
 
 let build_ltable ws = 
   let rec aux t = function
@@ -28,6 +37,40 @@ let build_ltable ws =
       | Not_found -> aux ((x, [y]) :: t) (y :: xs)
   in
   aux [("START", [List.hd ws])] ws
+;;
+
+let next_in_ltable table w =
+  let ws = List.assoc w table in 
+  List.nth ws (Random.int (List.length ws))
+;;
+
+let walk_ltable table = 
+  let rec aux w l =
+    if w = "STOP" then List.rev (List.tl l)
+    else 
+      let next = next_in_ltable table w in
+      aux next (next :: l)
+  in
+    aux "START" []
+;;
+
+
+(* using hash table *)
+
+let compute_distribution xs =
+  let counts = List.fold_left
+    (fun acc x ->
+       let (k, c) = List.hd acc in
+       if k = x 
+       then (k, c+1) :: List.tl acc 
+       else (x, 1) :: acc)
+    [(List.hd xs, 1)]
+    (List.sort Pervasives.compare (List.tl xs))
+  in
+  let total = counts
+              |> List.map (fun (k, v) -> v)
+              |> List.fold_left ( + ) 0 in
+  {total = total; amounts = counts }
 ;;
 
 (* returns the hashtable *)
@@ -51,32 +94,42 @@ let build_hashtable ws =
   let ht = Hashtbl.create (List.length ws) in
   Hashtbl.add ht "START" [List.hd ws];
   aux ht ws;
-  ht
+
+  let table : htable = Hashtbl.create (List.length ws) in
+  Hashtbl.iter 
+    (fun k v -> Hashtbl.add table k (compute_distribution v))
+    ht;
+  table
 ;;
 
 let table = build_hashtable (words text);;
 
-let next_in_ltable table w =
-  let ws = Hashtbl.find table w in
-  List.nth ws (Random.int (List.length ws))
+let next_in_htable table w =
+  let dist = Hashtbl.find table w in
+  let index = 1 + Random.int dist.total in
+  let rec aux total words =
+    let (w, c) = List.hd words in
+    if total + c >= index then w
+    else aux (total + c) (List.tl words)
+  in
+  aux 0 dist.amounts
 ;;
 
-let walk_table table = 
+let walk_htable table = 
   let rec aux w l =
     if w = "STOP" then List.rev (List.tl l)
     else 
-      let next = next_in_ltable table w in
+      let next = next_in_htable table w in
       aux next (next :: l)
   in
     aux "START" []
 ;;
 
-(* generate 10 sentences *)
 let generate_text table =
   let rec aux i l = 
     if i = 0 then l
     else 
-      aux (i-1) ((String.concat " " (walk_table table)) :: l)
+      aux (i-1) ((String.concat " " (walk_htable table)) :: l)
   in
   aux 10 []
 ;;
