d847a56383a291f00bcff79f033e97693fd622ea
Prakhar Srivastav <prakhar1989@gmail.com>
2016-01-27 14:30:06 -0500

    added the concrete type for median

--- misc/counter/counter.mli
+++ misc/counter/counter.mli
@@ -4,8 +4,16 @@ open Core.Std
 (** A collection of string frequence counts *)
 type t
 
+type median = 
+  | Median of string
+  | Before_and_after of string * string
+;;
+
 (** the empty set of frequency counts *)
 val empty : t
+
+(** computed median *)
+val median : t -> median
 
 (** bump the frequency count for the given string *)
 val touch : t -> string -> t
