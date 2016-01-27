c19a5a0c18ee72dcde64c9fe8e68dc88cff19fbb
Prakhar Srivastav <prakhar1989@gmail.com>
2016-01-27 14:18:23 -0500

    moved from assoc lists to maps

--- misc/counter/freq.ml
+++ misc/counter/freq.ml
@@ -7,7 +7,7 @@ let build_counts () =
 (* ensures the expression on the left returns unit *)
 let () =
   build_counts ()
-|> Counter.to_list
+  |> Counter.to_list
   |> List.sort ~cmp:(fun (_, x) (_, y) -> Int.descending x y)
   |> (fun l -> List.take l 10)
   |> List.iter ~f:(fun (line, count) -> printf "%3d: %s\n" count line)
